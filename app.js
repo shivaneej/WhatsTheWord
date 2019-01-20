8e16002ccf60451efc082753fde2ac2e7de3a600
Shivanee <Shivaneej02@gmail.com>
2019-01-20 23:55:19 +0530

    modify

--- app.js
+++ app.js
@@ -61,3 +61,25 @@ async function getData()
 	
 
 }
+
+window.addEventListener('load', async e => {
+    console.log(navigator.onLine);
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceworker.js');
+            console.log('SW registered');
+        } catch (error) {
+            console.log('SW failed');
+
+        }
+    }
+    if(navigator.onLine){
+        navigator.serviceWorker.controller.postMessage("online");
+    }
+    else
+    {
+        displayNotification('No Internet','Please connent to a network to search a new word');
+        navigator.serviceWorker.controller.postMessage("offline");
+    }
+    await fetchNew();
+});
